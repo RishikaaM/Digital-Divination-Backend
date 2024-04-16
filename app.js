05543ce1335c64fcd1a9786f2182706cce6626ec
RishikaaM <145793987+RishikaaM@users.noreply.github.com>
2024-04-16 08:51:22 +0530

    Divination Backend

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const divination = require("./divination.json");
+
+app.use(express.json());
+app.use(cors({
+    origin: ["http://127.0.0.1:5500", "http://localhost:5500"]
+}))
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+app.post("/getDivinationResult", (req, res) => {
+
+    const { name, selectedElement, areaOfLife } = req?.body;
+
+    const divinationResultsForElement = divination[selectedElement][areaOfLife];
+
+    const randomNumber = Math.floor(Math.random() * 10);
+
+    return res.json({
+        divinationResult: divinationResultsForElement?.[randomNumber]
+    })
+
+})
+
+app.listen(3000, () => {
+    console.log(`Server Started listening on: http://127.0.0.1:3000`);
+})
